c58ee18829f5b903c694bdbe30b45a9ca1b4e329
TEMPESTINI Terry <terry.tempestini@telecomnancy.eu>
2024-03-26 09:17:39 +0100

    add: handle response server

--- serveurTCP.c
+++ serveurTCP.c
@@ -3,8 +3,65 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdbool.h>
 
 #define PORT 8080
+
+typedef struct {
+    int port;
+    bool is_open;
+} port_info;
+
+void scan_vertical(char *ip_address, int start_port, int end_port, port_info *results) {
+    struct sockaddr_in sock_addr;
+    socklen_t sock_addr_len = sizeof(sock_addr);
+
+    for (int i = start_port; i <= end_port; i++) {
+        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0) {
+            perror("ERROR opening socket");
+            exit(1);
+        }
+
+        sock_addr.sin_family = AF_INET;
+        sock_addr.sin_port = htons(i);
+        inet_pton(AF_INET, ip_address, &sock_addr.sin_addr);
+
+        if (connect(sockfd, (struct sockaddr *)&sock_addr, sock_addr_len) == 0) {
+            results[i].port = i;
+            results[i].is_open = true;
+            printf("Port %d is open\n", i);
+        } else {
+            results[i].port = i;
+            results[i].is_open = false;
+            printf("Port %d is closed\n", i);
+        }
+
+        close(sockfd);
+    }
+}
+
+void handleClientRequest(char *request, char *response) {
+    switch(atoi(request)) {
+        case 1:
+            strcpy(response, "SCAN_H\n");
+            break;
+        case 2:
+            strcpy(response, "SCAN_V\n");
+            break;
+        case 3:
+            strcpy(response, "HELP\n");
+            break;
+        case 4:
+            strcpy(response, "QUIT\n");
+            break;
+        default:
+            strcpy(response, "Commande invalide.\n");
+            break;
+    }
+}
 
 int main() {
     int server_fd, new_socket;
@@ -44,6 +101,10 @@ int main() {
     while(1) {
         memset(buffer, 0, sizeof(buffer)); // Nettoyer le buffer
         int valread = read(new_socket, buffer, 1024);
+
+        char *response = (char *)malloc(1024);
+        handleClientRequest(buffer, response);
+
         if(valread <= 0) {
             printf("Connexion fermée par le client.\n");
             break;
@@ -51,10 +112,11 @@ int main() {
         printf("Client : %s\n",buffer);
 
         // Envoyer une réponse au client
-        char *response = "Message reçu par le serveur.";
         send(new_socket, response, strlen(response), 0);
 
         if(strcmp(buffer, "fin") == 0) break; // Si le message est "fin", arrêtez le serveur.
+
+        free(response);
     }
 
     close(new_socket);
